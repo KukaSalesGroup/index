063658e38b444ef721e36df1f765c1d653c60766
IT Department <kukasalesgroup.it@gmail.com>
2020-06-17 13:33:10 -0600

    Inegration test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,7 @@ import PropTypes from 'prop-types'
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         <p className="copyright">&copy; All Rights Reserved<br>
-        </br>Kuka Sales Group prices are subject to change. Verify at time of order.
-        <br>TEST TO CONFIRM INTEGRATION</br>
+        </br>Kuka Sales Group prices are subject to change. Verify at time of order. THIS IS AN INERGRATION TEST
         </p>
     </footer>
 )
