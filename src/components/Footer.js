4e49d307170ea867456a460f4b68264c7275b56f
IT Department <kukasalesgroup.it@gmail.com>
2020-06-17 12:01:13 -0600

    Integration test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         <p className="copyright">&copy; All Rights Reserved<br>
         </br>Kuka Sales Group prices are subject to change. Verify at time of order.
+        <br>TEST TO CONFIRM INTEGRATION</br>
         </p>
     </footer>
 )
