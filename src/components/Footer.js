d160ab72a114c6c9a76d6733b9bfdc9000f1f1f2
Shawn McCrum <xmccrum@gmail.com>
2020-06-19 06:55:28 -0600

    Initial commit from associated account

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         <p className="copyright">&copy; All Rights Reserved<br>
-        </br>Kuka Sales Group prices are subject to change. Verify at time of order. THIS IS AN INERGRATION TEST
+        </br>Kuka Sales Group prices are subject to change. Verify at time of order. THIS IS AN INERGRATION TEST FROM ANOTHER ACCOUNT
         </p>
     </footer>
 )
