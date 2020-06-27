eb4a34b87460d649d3d8e921c4827fe99f49b959
Shawn McCrum <xmccrum@gmail.com>
2020-06-27 16:40:38 -0600

    Commented out unused buttons. Formatted header text layoutt

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,34 @@ const Header = props => (
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Kuka Sales Group</h1>
+
+        <h1>
+          Kuka Sales Group
+        </h1>
+
         <p>
-          <a href="tel:3038401047">
-                (303)-840-1047
-          </a><br />
-          7108 S. Alton Way<br />
+          Located at: 7108 S. Alton Way<br />
           Bldg. F, Unit 1<br />
-          Centennial, CO 80112
-        </p>                
+          Centennial, CO 80112<br />
+        </p>
+
+        <p>  
+          <a href="tel:3038401047">
+            Call: (303)-840-1047
+          </a><br />
+          
+          <a href="mailto:jkuka@kukasales.com">
+            Email: Joe Kuka
+          </a><br /> 
+
+          <a href="mailto:gkuka@kukasales.com">
+            Email: Garry Kuka
+          </a><br />
+
+          <a href="mailto:sales@kukasales.com?subject=July Special">
+            Email: Sales Team
+          </a><br />
+        </p>   
       </div>
     </div>
     <nav>
@@ -27,16 +46,7 @@ const Header = props => (
               props.onOpenArticle('intro')
             }}
           >
-            july special
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onOpenArticle('work')
-            }}
-          >
-            Products
+            july specials
           </button>
         </li>
         <li>
@@ -45,9 +55,18 @@ const Header = props => (
               props.onOpenArticle('about')
             }}
           >
-            About Us
+            about kuka
           </button>
         </li>
+{/*        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Products
+          </button>
+        </li>        
         <li>
           <button
             onClick={() => {
@@ -66,6 +85,8 @@ const Header = props => (
             Blog
           </button>
         </li>        
+          */}
+
       </ul>
     </nav>
   </header>
